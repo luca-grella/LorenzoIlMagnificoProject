d92e6defb1d26eacb63e2683adb917e774dbc373
Luca Grella <luca.grella@mail.polimi.it>
2017-05-24 22:54:52 +0200

    Modifica pom + inserimento cards json

---
+++ src/test/java/it/polimi/ingsw/ps18/jasoncardstest.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.ps18;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+public class jasoncardstest {
+	
+	
+	public static void main(String[] args) {
+	JSONParser parser = new JSONParser();
+
+    try {
+
+        Object obj = parser.parse(new FileReader("ps18/src/test/java/it/polimi/ingsw/ps18/cards.json"));
+
+        JSONObject jsonObject = (JSONObject) obj;
+        System.out.println(jsonObject);
+
+        String name = (String) jsonObject.get("name");
+        System.out.println(name);
+
+       
+    }catch (FileNotFoundException e) {
+        e.printStackTrace();
+
+} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (org.json.simple.parser.ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
