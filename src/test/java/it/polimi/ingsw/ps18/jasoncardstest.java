d08f298e8792cbefc3e44ecd9e649f37253c256b
Luca Grella <luca.grella@mail.polimi.it>
2017-05-26 22:59:18 +0200

    up json

--- src/test/java/it/polimi/ingsw/ps18/jasoncardstest.java
+++ src/test/java/it/polimi/ingsw/ps18/jasoncardstest.java
@@ -18,14 +18,10 @@ public class jasoncardstest {
 
     try {
     	
-    	FileReader f = new FileReader("src\\test\\java\\it\\polimi\\ingsw\\ps18\\cards.json");
+    	FileReader f = new FileReader("src/main/java/it/polimi/ingsw/ps18/Model/Cards/cards.json");
 
-<<<<<<< 067aa5cde749c25182a44e9d3da2c60e5d23925b
-        Object obj = parser.parse(new FileReader("/Users/luca/LorenzoIlMagnificoProject/src/main/java/it/polimi/ingsw/ps18/Model/Cards/cards.json"));
-=======
-        Object obj = parser.parse(f);
-        
->>>>>>> d2cf374d066484e0ea053973e666e639d92763e1
+        Object obj = parser.parse(new FileReader("src/main/java/it/polimi/ingsw/ps18/Model/Cards/cards.json"));
+
 
         JSONObject jsonObject = (JSONObject) obj;
         //System.out.println(jsonObject);
