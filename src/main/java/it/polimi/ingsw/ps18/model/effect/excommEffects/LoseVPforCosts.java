559fdf226950d3be281819fb107c1609cb9ef6a2
Luca Grella <luca.grella@mail.polimi.it>
2017-06-18 19:15:36 +0200

    bug fix
    -sistemato il problema dell’excommunication dock che non si creava
    correttamente

--- src/main/java/it/polimi/ingsw/ps18/model/effect/excommEffects/LoseVPforCosts.java
+++ src/main/java/it/polimi/ingsw/ps18/model/effect/excommEffects/LoseVPforCosts.java
@@ -2,8 +2,10 @@ package it.polimi.ingsw.ps18.model.effect.excommEffects;
 
 public class LoseVPforCosts implements ExcommEffects {
 
+	
 	private int loseVP;
 	private String name = "Lose Victory Point for each wood or rock represented on each yellow card in your PBoard";
+	
 	
 	@Override
 	public void setQuantity(int quantity) {
